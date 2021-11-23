b7254d1ee8cd9b34bdd5c3eb22b3ef98ccd5db9f
gaomeng1900 <gaomeng1900@gmail.com>
2021-11-24 01:01:47 +0800

    three lite backend init

---
+++ packages/backend/threelite/src/PrgSpriteMaterial.ts
@@ -0,0 +1,141 @@
+/**
+ * Copyright (C) 2021 Alibaba Group Holding Limited
+ * All rights reserved.
+ */
+
+import {
+	MeshBasicMaterial,
+	UniformsUtils,
+	Color,
+	Matrix3,
+	Vector2,
+} from '@gs.i/three-lite-renderer'
+import { MatrSpriteDataType } from '@gs.i/schema'
+import { genPreShaderCode } from './utils'
+import vs from './PrgSpriteMatr.vs.glsl'
+import fs from './PrgSpriteMatr.fs.glsl'
+
+export class PrgSpriteMaterial extends MeshBasicMaterial {
+	type = 'ShaderMaterial'
+
+	// Add ShaderMaterial flag
+	isShaderMaterial = true
+
+	defines: { [name: string]: any } = {}
+	uniforms = {
+		diffuse: { value: new Color(0xffffff) },
+		opacity: { value: 1.0 },
+		// center: { value: new THREE.Vector2(0.5, 0.5) },
+		// rotation: { value: 0.0 },
+		map: { value: null },
+		alphaMap: { value: null },
+		uvTransform: { value: new Matrix3() },
+		uCenter: { value: new Vector2(0.5, 0.5) },
+		uSize: { value: new Vector2(1.0, 1.0) },
+		uRotation: { value: 0.0 },
+	}
+
+	vertexShader = ''
+	fragmentShader = ''
+
+	linewidth = 1
+
+	wireframe = false
+	wireframeLinewidth = 1
+
+	fog = false // set to use scene fog
+	lights = false // set to use scene lights
+	clipping = false // set to use user-defined clipping planes
+
+	skinning = false // set to use skinning attribute streams
+	morphTargets = false // set to use morph targets
+	morphNormals = false // set to use morph normals
+
+	extensions = {
+		derivatives: true, // set to use derivatives
+		fragDepth: true, // set to use fragment depth values
+		drawBuffers: true, // set to use draw buffers
+		shaderTextureLOD: true, // set to use shader texture LOD
+	}
+
+	// When rendered geometry doesn't include these attributes but the material does,
+	// use these default values in WebGL. This avoids errors when buffer data is missing.
+	defaultAttributeValues = {
+		position: [0, 0, 0],
+		normal: [0, 0, 0],
+		color: [1, 1, 1],
+		uv: [0, 0],
+		uv2: [0, 0],
+	}
+
+	index0AttributeName = undefined
+	uniformsNeedUpdate = false
+
+	glslVersion = null
+
+	constructor(gsiMatr: MatrSpriteDataType) {
+		super()
+		// this.setValues(gsiMatr as any) // Set parent class parameters
+
+		if (!gsiMatr.language) {
+			gsiMatr.language = 'GLSL300'
+		} else if (gsiMatr.language !== 'GLSL100' && gsiMatr.language !== 'GLSL300') {
+			throw new Error(`GSI::Matr.language type '${gsiMatr.language}' is not supported`)
+		}
+
+		// Set shader codes
+		const preCodes = genPreShaderCode(gsiMatr)
+
+		this.vertexShader = vs
+			.replace(/\$\$GSI_INSERT<Attributes>/g, preCodes.attributes || '')
+			.replace(/\$\$GSI_INSERT<Varyings>/g, preCodes.varyingsVert || '')
+			.replace(/\$\$GSI_INSERT<Uniforms>/g, preCodes.uniforms || '')
+			.replace(/\$\$GSI_INSERT<PreVert>/g, gsiMatr.preVert || '')
+			.replace(/\$\$GSI_INSERT<VertGeometry>/g, gsiMatr.vertGeometry || '')
+			.replace(/\$\$GSI_INSERT<VertOutput>/g, gsiMatr.vertOutput || '')
+			.replace(/\$\$GSI_INSERT<PostVert>/g, gsiMatr.postVert || '')
+
+		this.fragmentShader = fs
+			.replace(/\$\$GSI_INSERT<Varyings>/g, preCodes.varyingsFrag || '')
+			.replace(/\$\$GSI_INSERT<Uniforms>/g, preCodes.uniforms || '')
+			.replace(/\$\$GSI_INSERT<PreFrag>/g, gsiMatr.preFrag || '')
+			.replace(/\$\$GSI_INSERT<FragColor>/g, gsiMatr.fragColor || '')
+
+		if (gsiMatr.useAttrSize) {
+			this.defines.GSI_USE_ATTR_SIZE = true
+		}
+
+		// this.onBeforeCompile = (shader) => {
+		// 	shader['extensionDerivatives'] = true
+		// 	shader['extensionShaderTextureLOD'] = true
+		// }
+	}
+
+	copy(source) {
+		MeshBasicMaterial.prototype.copy.call(this, source)
+
+		this.fragmentShader = source.fragmentShader
+		this.vertexShader = source.vertexShader
+
+		this.uniforms = UniformsUtils.clone(source.uniforms)
+
+		this.defines = Object.assign({}, source.defines)
+
+		this.wireframe = source.wireframe
+		this.wireframeLinewidth = source.wireframeLinewidth
+
+		this.lights = source.lights
+		this.clipping = source.clipping
+
+		this.skinning = source.skinning
+
+		this.morphTargets = source.morphTargets
+		this.morphNormals = source.morphNormals
+
+		this.extensions = Object.assign({}, source.extensions)
+
+		this.glslVersion = source.glslVersion
+
+		return this
+	}
+}
---
+++ packages/backend/threelite/src/PrgSpriteMatr.fs.glsl
@@ -0,0 +1,35 @@
+uniform vec3 diffuse;
+uniform float opacity;
+uniform vec2 uCenter;
+uniform float uRotation;
+uniform vec2 uSize;
+varying vec2 vUv;
+uniform mat3 uvTransform;
+#include <common>
+// #include <uv_pars_fragment>
+#include <map_pars_fragment>
+#include <alphamap_pars_fragment>
+#include <fog_pars_fragment>
+// #include <logdepthbuf_pars_fragment>
+// #include <clipping_planes_pars_fragment>
+$$GSI_INSERT<Varyings>
+$$GSI_INSERT<Uniforms>
+$$GSI_INSERT<PreFrag>
+void GSI_FUNC_FragColor(inout vec4 fragColor) {
+    $$GSI_INSERT<FragColor>
+}
+void main() {
+	// #include <clipping_planes_fragment>
+	vec3 outgoingLight = vec3( 0.0 );
+	vec4 diffuseColor = vec4( diffuse, opacity );
+	// #include <logdepthbuf_fragment>
+	#include <map_fragment>
+	#include <alphamap_fragment>
+	#include <alphatest_fragment>
+	outgoingLight = diffuseColor.rgb;
+	gl_FragColor = vec4( outgoingLight, diffuseColor.a );
+	GSI_FUNC_FragColor(gl_FragColor);
+	#include <tonemapping_fragment>
+	#include <encodings_fragment>
+	#include <fog_fragment>
+}
