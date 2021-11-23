5a2f7075168380632bec26af1db3cee0995488e7
gaomeng1900 <gaomeng1900@gmail.com>
2021-11-24 00:44:20 +0800

    Semantic rendering description as ext schema

---
+++ packages/schema/ext-scene/src/Postprocessing.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright (C) 2021 Alibaba Group Holding Limited
+ * All rights reserved.
+ */
+
+/**
+ * 后处理Pass定义
+ * @typedef name
+ * @typedef props
+ */
+export type Postprocessing = { name: string; props: { [key: string]: any } }
