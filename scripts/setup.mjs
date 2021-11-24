e60c9851c8e6d45397afd030acd0193a65b156e0
gaomeng1900 <gaomeng1900@gmail.com>
2021-11-25 01:00:19 +0800

    update setup logic

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -8,10 +8,22 @@ import { execSync, spawn, exec, execFileSync } from 'child_process'
 console.log(argv)
 console.log(process.env.PWD)
 
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+console.log(__dirname)
+
 // # https://github.com/lerna/lerna/issues/1421
 // # ⬆️ bad bad design
 // # lerna 的 --ignore 和 --no-private 并不会生效
 // # 因此只能自己处理，先把不需要的 package 的 dependents 换成空的，绕开 lerna 的逻辑，然后再恢复
+
+const id = Math.floor(Math.random() * 9999) + ''
+console.log(id)
+
+execSync(`node ${path.resolve(__dirname, './packageJsonBackup.mjs')} --id=${id}`, {
+	stdio: 'inherit',
+})
 
 import blist from './blacklist.mjs'
 
@@ -38,12 +50,9 @@ const packages = packageALL.filter((pac) => {
 })
 console.log(packages)
 
-const changedFiles = []
-
 for (const pkg of packages) {
 	console.log('ignore package: ', pkg.name)
 	const pjsonPath = path.resolve(pkg.location, 'package.json')
-	const pjsonBacPath = path.resolve(pkg.location, 'package.json.bac')
 
 	const pjson = await readFile(pjsonPath)
 	const originalJson = JSON.parse(pjson)
@@ -59,15 +68,8 @@ for (const pkg of packages) {
 	// backup
 
 	try {
-		console.log('临时修改 pjson ', pjsonPath, '->', pjsonBacPath)
-		await rename(pjsonPath, pjsonBacPath)
 		await writeFile(pjsonPath, JSON.stringify(originalJson))
-
-		changedFiles.push([pjsonPath, pjsonBacPath])
 	} catch (error) {
-		await unlink(pjsonPath)
-		await rename(pjsonBacPath, pjsonPath)
-
 		console.error(error)
 		console.log(pkg.name, tsconfigPath)
 	}
@@ -82,23 +84,13 @@ try {
 
 	// # should not hoist local packages
 	execSync(`rm -rf ./node_modules/@gs.i`, { stdio: 'inherit' })
-
-	for (const changedFile of changedFiles) {
-		const [pjsonPath, pjsonBacPath] = changedFile
-
-		await unlink(pjsonPath)
-		await rename(pjsonBacPath, pjsonPath)
-	}
 } catch (error) {
-	for (const changedFile of changedFiles) {
-		const [pjsonPath, pjsonBacPath] = changedFile
-
-		await unlink(pjsonPath)
-		await rename(pjsonBacPath, pjsonPath)
-	}
-
 	console.error(error)
 }
+
+execSync(`node ${path.resolve(__dirname, './packageJsonRestore.mjs')} --id=${id}`, {
+	stdio: 'inherit',
+})
 
 console.log('本次安装不会安装以下package的依赖，如果需要，可以自行到文件夹中安装')
 console.log(packageBlacklist)
