76986c68f977182f33262df3cd88de3fff7ff434
gaomeng1900 <gaomeng1900@gmail.com>
2021-12-08 22:44:14 +0800

    use standard es module

--- packages/backend/gltf2/scripts/rebuild.sh
+++ packages/backend/gltf2/scripts/rebuild.sh
@@ -12,3 +12,12 @@
 # tsc --project tsconfig.build.json --incremental
 
 npx tsc --project tsconfig.build.json
+
+# tsc output es module codes in .js files
+# which WILL NOT BREAK in nodejs env
+# > SyntaxError: The requested module '***' is expected to be of type CommonJS, which does not support named exports.
+# add 'type' 'module' properties in package.json fix this.
+# but es modules require that `import` must have extension
+# so fix it here
+
+npx node ./scripts/fixTscExtension.mjs
