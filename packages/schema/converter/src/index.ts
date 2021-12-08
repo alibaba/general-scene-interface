f10a554580cabd4e265c378f958d94d97c576f8c
gaomeng1900 <gaomeng1900@gmail.com>
2021-12-08 22:55:45 +0800

    clean up

--- packages/schema/converter/src/index.ts
+++ packages/schema/converter/src/index.ts
@@ -3,5 +3,4 @@
  * All rights reserved.
  */
 
-// import { Range, UpdateRanges, BBox, BSphere, Vec4, Vec3, Vec2, ColorLike, TypedArray, GLSL_CODE, isGLSL, readonly } from './basic'
 export * from './Converter'
