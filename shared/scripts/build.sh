697639fa1b445fd0231a6039051c3568c6346d96
gaomeng1900 <gaomeng1900@gmail.com>
2021-12-20 23:37:41 +0800

    stage

--- shared/scripts/build.sh
+++ shared/scripts/build.sh
@@ -3,35 +3,43 @@
 # DO NOT EDIT
 # AUTO COPIED FROM ROOT/shared
 
-# npx tsc --project tsconfig.build.json
-# ⬇️ much faster
-if [ -e .cached-built-head &&  ]
+build () {
+	echo building
+
+	# npx tsc --project tsconfig.build.json
+	# ⬇️ much faster
+	./node_modules/.bin/tsc --project tsconfig.build.json
+
+	# tsc output es module codes in .js files
+	# which WILL NOT BREAK in nodejs env
+	# > SyntaxError: The requested module '***' is expected to be of type CommonJS, which does not support named exports.
+	# add 'type' 'module' properties in package.json fix this.
+	# but es modules require that `import` must have extension
+	# so fix it here
+
+	# npx node ./scripts/fixTscExtension.mjs
+	# ⬇️ much faster
+	node ./scripts/fixTscExtension.mjs
+
+	echo `git rev-parse --short HEAD` > .cached-built-head
+}
+
+if [ -e .cached-built-head ]
 
 then
-	builtHead=$(.cached-built-head)
+	builtHead=$(<.cached-built-head)
 
-	if [`git diff --quiet $builtHead -- ./ || echo changed` == "changed"]
+	if [ `git diff --quiet $builtHead -- ./ || echo "true"` ]
 	then
-		echo "changed"
-		# ./node_modules/.bin/tsc --project tsconfig.build.json
-		echo `git rev-parse --short HEAD` > .cached-built-head
+		echo changed
+		build
 	else
-		echo "not changed"
+		echo not-changed
 	fi
 else
-	echo "first build"
-	# ./node_modules/.bin/tsc --project tsconfig.build.json
-	echo `git rev-parse --short HEAD` > .cached-built-head
+	echo first-build
+	build
+	
 fi
 
 
-# tsc output es module codes in .js files
-# which WILL NOT BREAK in nodejs env
-# > SyntaxError: The requested module '***' is expected to be of type CommonJS, which does not support named exports.
-# add 'type' 'module' properties in package.json fix this.
-# but es modules require that `import` must have extension
-# so fix it here
-
-# npx node ./scripts/fixTscExtension.mjs
-# ⬇️ much faster
-node ./scripts/fixTscExtension.mjs
