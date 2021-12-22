479c0989a11e50a0072b6c01d90c7e674fde83ff
gaomeng1900 <gaomeng1900@gmail.com>
2021-12-23 00:24:21 +0800

    increment build

--- packages/processors/graph/scripts/build.sh
+++ packages/processors/graph/scripts/build.sh
@@ -3,13 +3,50 @@
 # DO NOT EDIT
 # AUTO COPIED FROM ROOT/shared
 
-npx tsc --project tsconfig.build.json
+build () {
+	echo building
 
-# tsc output es module codes in .js files
-# which WILL NOT BREAK in nodejs env
-# > SyntaxError: The requested module '***' is expected to be of type CommonJS, which does not support named exports.
-# add 'type' 'module' properties in package.json fix this.
-# but es modules require that `import` must have extension
-# so fix it here
+	# npx tsc --project tsconfig.build.json
+	# ⬇️ much faster
+	./node_modules/.bin/tsc --project tsconfig.build.json
 
-npx node ./scripts/fixTscExtension.mjs
+	# tsc output es module codes in .js files
+	# which WILL NOT BREAK in nodejs env
+	# > SyntaxError: The requested module '***' is expected to be of type CommonJS, which does not support named exports.
+	# add 'type' 'module' properties in package.json fix this.
+	# but es modules require that `import` must have extension
+	# so fix it here
+
+	# npx node ./scripts/fixTscExtension.mjs
+	# ⬇️ much faster
+	node ./scripts/fixTscExtension.mjs
+
+	# fix: clean build, dirt rebuild, clean again, should rebuild anyway
+	# if folder is dirty, should delete cache file
+	if [ -z `git status --porcelain -- ./` ] # True if the string is empty
+	then 
+		echo `git rev-parse --short HEAD` > .cached-built-head
+	else
+		rm -f .cached-built-head
+	fi
+}
+
+if [ -e .cached-built-head ]
+
+then
+	builtHead=$(<.cached-built-head)
+
+	if [ `git diff --quiet $builtHead -- ./ || echo "true"` ]
+	then
+		echo changed
+		build
+	else
+		echo not-changed
+	fi
+else
+	echo first-build
+	build
+	
+fi
+
+
