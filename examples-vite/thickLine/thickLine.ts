75a6a39aeacbb5ad332abedb992296de2db50741
gaomeng.gm <gaomeng.gm@alibaba-inc.com>
2022-02-25 21:15:15 +0800

    add package `thick-line-builder`

---
+++ examples-vite/thickLine/thickLine.ts
@@ -0,0 +1,75 @@
+import { uvMaterial } from '@gs.i/frontend-materials'
+import { buildThickLine } from '@gs.i/thick-line-builder'
+import IR from '@gs.i/schema-scene'
+import { specifyNode } from '@gs.i/utils-specify'
+import { ThreeLiteConverter } from '@gs.i/backend-threelite'
+import { IndicatorProcessor } from '@gs.i/processor-indicator'
+
+import { renderer, timeline, camera, cameraProxy, WIDTH, HEIGHT } from '../__utils/LiteRenderer'
+import { scene as threeScene } from '../__utils/LiteScene'
+
+{
+	const scene = specifyNode({
+		material: uvMaterial,
+		geometry: buildThickLine(
+			[
+				{ x: 0, y: 0 },
+				{ x: 100, y: 200 },
+				{ x: 200, y: -200 },
+				{ x: 201, y: -100 },
+				{ x: 300, y: 0 },
+			],
+			// { uv: true, width: 10, joint: 'bevel' }
+			{ uv: true, width: 10, joint: 'round' }
+			// { uv: true, width: 10, joint: 'miter' }
+		) as IR.Geometry,
+	}) as IR.RenderableNode
+	console.log(scene)
+
+	const indicator = new IndicatorProcessor({
+		// hideOriginal: true,
+		useBBox: true,
+		// useBSphere: true,
+		useWireframe: true,
+	})
+	indicator.traverse(scene)
+
+	const conv = new ThreeLiteConverter({ overrideFrustumCulling: true })
+	console.log(conv)
+	window['conv'] = conv
+
+	// debugger
+	const threeMesh = conv.convert(scene)
+	console.log(threeMesh)
+
+	// console.log(cameraControl)
+
+	threeScene.add(threeMesh)
+	timeline.add({
+		duration: Infinity,
+		onUpdate: () => {
+			// scene.transform.version++
+
+			// TODO should check if this needs to be updated
+			conv.cullingProcessor.updateFrustum({
+				cameraPosition: camera.position,
+				cameraRotation: camera.rotation,
+				cameraNear: camera.near,
+				cameraFar: camera.far,
+				cameraFOV: camera.fov,
+				cameraAspect: camera.aspect,
+			})
+
+			conv.convert(scene)
+			renderer.render(threeScene, camera)
+		},
+	})
+
+	timeline.updateMaxFPS(60)
+
+	timeline.play()
+
+	window['cameraProxy'] = cameraProxy
+}
+
+export {}
---
+++ packages/utils/thick-line-builder/README.md
@@ -0,0 +1,7 @@
+# Thick Line Builder
+
+Extend line strings into triangle meshes with **uv**, dynamic width and **miter/bevel joint**.
+
+This generates "noodle-like" meshes, with no thickness, that always face the positive z-axis.
+
+If you want thick lines that always face the camera (extended in screen space). You should use `@gs.i/frontend-gline`. Gline generates thick lines with **miter and bevel joint** in shaders.
